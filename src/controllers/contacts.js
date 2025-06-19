7fff742a2889f08d52044d804fdac837f37cc958
Makson <144148309+Sky65678@users.noreply.github.com>
2025-06-20 00:52:44 +0300

    add dependenses contId and fix email

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,3 +1,5 @@
+import * as fs from 'node:fs/promises';
+
 import {
   getContacts,
   getContactsById,
@@ -53,13 +55,19 @@ async function getContactByIdController(req, res) {
 }
 
 async function createContactController(req, res) {
-  console.log(req.file);
+  let photoUrl = null;
 
-  const result = await uploadToCloudinary(req.file.path);
+  if (req.file) {
+    const result = await uploadToCloudinary(req.file.path);
+    await fs.unlink(req.file.path);
+    photoUrl = result.secure_url;
+  }
 
-  console.log(result);
-
-  const contact = await createContact({ ...req.body, userId: req.user.id });
+  const contact = await createContact({
+    ...req.body,
+    userId: req.user.id,
+    photo: photoUrl,
+  });
 
   res.status(201).json({
     status: 201,
@@ -70,8 +78,23 @@ async function createContactController(req, res) {
 
 async function updateContactController(req, res) {
   const { id } = req.params;
+  let photoUrl = null;
 
-  const contact = await updateContact(id, req.body, req.user.id);
+  if (req.file) {
+    const result = await uploadToCloudinary(req.file.path);
+    await fs.unlink(req.file.path);
+    photoUrl = result.secure_url;
+  }
+
+  const updatedData = {
+    ...req.body,
+  };
+
+  if (photoUrl) {
+    updatedData.photo = photoUrl;
+  }
+
+  const contact = await updateContact(id, updatedData, req.user.id);
 
   if (contact === null) {
     throw createError(404, 'Contact not found');
